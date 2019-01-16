8d5eead96f381561ff5d8c3038c0d7c44c095ea4
Sébastien Romain <sromain@tlnd-sromain.local>
2019-01-16 13:36:54 +0100

    update

---
+++ src/env.js
@@ -0,0 +1,2 @@
+export const SLACK_TOKEN_API = process.env.SLACK_TOKEN_API;
+export const GITHUB_TOKEN_API = process.env.GITHUB_TOKEN_API;
--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import configurations from "./configurations";
 import { getPrsFromRepository } from "./github";
 import { sendPrsToSlack } from "./slack";
 
-configurations.forEach(configuration => {
-  Promise.all(
-    configuration.repositories.map(repository =>
-      getPrsFromRepository(repository, configuration.users)
-    )
-  ).then(values => {
-    sendPrsToSlack(values, configuration);
+export const run = function() {
+  configurations.forEach(configuration => {
+    Promise.all(
+      configuration.repositories.map(repository =>
+        getPrsFromRepository(repository, configuration.users)
+      )
+    ).then(values => {
+      sendPrsToSlack(values, configuration);
+    });
   });
-});
+};

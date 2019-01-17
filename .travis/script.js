284ddfee899c55b2c0b1cf20a94ce79f01739086
Sébastien Romain <sromain@tlnd-sromain.local>
2019-01-17 11:49:54 +0100

    update travis configuration

---
+++ .travis/script.js
@@ -0,0 +1,31 @@
+const util = require("util")
+const childProcess = require("child_process")
+const fs = require("fs")
+
+const fs_writeFile = util.promisify(fs.writeFile)
+const exec = util.promisify(childProcess.exec)
+
+const package = require("../package.json")
+const packageCurrentVersion = package.version
+const packageName = package.name
+
+async function writeNpmrcFile() {
+  const npmrcToken = process.env.NPMRC_TOKEN
+  fs_writeFile("./.npmrc", `//registry.npmjs.org/:_authToken=${npmrcToken}`)
+}
+
+async function executeShellCommand(shellCommand, trowError = true) {
+  const { stdout, stderr } = await exec(shellCommand)
+  if (stderr && trowError) throw stderr
+  return stdout
+}
+
+async function main() {
+  const values = await executeShellCommand(`npm view ${packageName} versions`)
+  if (!values.includes(packageCurrentVersion)) {
+    await writeNpmrcFile()
+    await executeShellCommand(`yarn publish`)
+  }
+}
+
+main()
